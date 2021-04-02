7f38b30b835c18c78a59afecb22c0fa49eff3294
chakib.daii <chakib.daii@proxym-it.com>
2021-04-02 23:34:02 +0100

    fixed the project structure + poms.

---
+++ src/main/java/com/example/camunda/extension/CamundaExtensionApplication.java
@@ -0,0 +1,13 @@
+package com.example.camunda.extension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaExtensionApplication {
+
+  public static void main(String... args) {
+    SpringApplication.run(CamundaExtensionApplication.class, args);
+  }
+
+}
